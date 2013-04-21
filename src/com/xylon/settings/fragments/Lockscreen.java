a934e24259bccdf7dae895e2e75b551c576f0f96
Daniel <daxx4182@gmail.com>
2013-04-21 12:33:16 +0800

    Settings: Lockscreen Tint Color

--- src/com/xylon/settings/fragments/Lockscreen.java
+++ src/com/xylon/settings/fragments/Lockscreen.java
@@ -68,6 +68,7 @@ public class Lockscreen extends SettingsPreferenceFragment implements Preference
     private static final String PREF_LOCK_CLOCK = "lock_clock";
     private static final String PREF_SEE_TRHOUGH = "see_through";
     private static final String BACKGROUND_PREF = "lockscreen_background";
+    private static final String PREF_LS_COLOR_ALPHA = "lock_color_alpha";
 
     private static final int REQUEST_CODE_BG_WALLPAPER = 199;
 
@@ -75,8 +76,7 @@ public class Lockscreen extends SettingsPreferenceFragment implements Preference
     private static final int LOCKSCREEN_BACKGROUND_CUSTOM_IMAGE = 1;
     private static final int LOCKSCREEN_BACKGROUND_DEFAULT_WALLPAPER = 2;
 
-    ListPreference mCustomBackground;
-    ListPreference mBatteryStatus;
+    ColorPickerPreference mLsColorAlpha;
     CheckBoxPreference mLockscreenAutoRotate;
     CheckBoxPreference mLockscreenAllWidgets;
     CheckBoxPreference mLockscreenUnlimitedWidgets;
@@ -85,6 +85,8 @@ public class Lockscreen extends SettingsPreferenceFragment implements Preference
     CheckBoxPreference mLockscreenLongpressChallenge;
     CheckBoxPreference mLockscreenUseCarousel;
     CheckBoxPreference mSeeThrough;
+    ListPreference mCustomBackground;
+    ListPreference mBatteryStatus;
 
     private File mWallpaperImage;
     private File mWallpaperTemporary;
@@ -138,6 +140,9 @@ public class Lockscreen extends SettingsPreferenceFragment implements Preference
 
         mCustomBackground = (ListPreference) prefSet.findPreference(BACKGROUND_PREF);
         mCustomBackground.setOnPreferenceChangeListener(this);
+
+        mLsColorAlpha = (ColorPickerPreference) findPreference(PREF_LS_COLOR_ALPHA);
+        mLsColorAlpha.setOnPreferenceChangeListener(this);
 
         mWallpaperImage = new File(getActivity().getFilesDir() + "/lockwallpaper");
         mWallpaperTemporary = new File(getActivity().getCacheDir() + "/lockwallpaper.tmp");
@@ -266,6 +271,14 @@ public class Lockscreen extends SettingsPreferenceFragment implements Preference
         } else if (preference == mCustomBackground) {
             int selection = mCustomBackground.findIndexOfValue(objValue.toString());
             return handleBackgroundSelection(selection);
+        } else if (preference == mLsColorAlpha) {
+            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                    .valueOf(objValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCKSCREEN_COLOR_ALPHA, intHex);
+            return true;
         }
         return false;
     }
