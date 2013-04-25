335c942217764bfff571397e95db2eee3352d51a
Jubakuba <Jubakuba@gmail.com>
2013-04-25 00:05:49 -0600

    Fix CustomToggle Actions as well.
    Didn't notice this, was building against an old sync.
    
    Change-Id: I8076860512341340c0e8b81d9b78c596a025508a

--- src/com/xylon/settings/fragments/QuickToggles.java
+++ src/com/xylon/settings/fragments/QuickToggles.java
@@ -707,9 +707,9 @@ public class QuickToggles extends SettingsPreferenceFragment implements
             mPicker.pickShortcut();
         } else { // This should be any other defined action.
             if (button.getPickLongPress()) {
-                button.setLongPress(AwesomeConstants.AwesomeActions()[command]);
+                button.setLongPress(mActionCodes[command]);
             } else {
-                button.setClickAction(AwesomeConstants.AwesomeActions()[command]);
+                button.setClickAction(mActionCodes[command]);
             }
         }
         refreshButtons();
